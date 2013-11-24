b619f04c91a08b67d07e84cca3af9e2c5c2e5d68
bruno2ms <br2msi@gmail.com>
2013-11-24 12:57:54 -0200

    Inicio estrutura do JS

---
+++ Content/js/main.js
@@ -0,0 +1,20 @@
+var geral = geral || {};
+
+geral = {
+	init: function(){
+		this.addEvents();
+	},
+	addEvents: function(){
+		this.adicionarToggleAoMenu();
+	},
+	adicionarToggleAoMenu: function(){
+		$("#menu-principal").on("click", "a", function(){
+			$(this).siblings("ul").collapse("toggle");
+		});
+	}
+
+};
+
+$(function(){
+	geral.init();
+});
